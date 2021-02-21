16cf49e5f7586e2a6aad8bf1cd8e73fd71da2fa2
Prasad Thakur <prasad.thakur@sap.com>
2021-02-20 17:13:07 -0800

    Updates

--- src/components/shared/TTBLogo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const TTBLogo = (props) => {
-    return (
-        <>
-            <img src="https://turnthebus.org/images/turn-the-bus-logo.png" alt="Seattle Public Library Logo" />
-        </>
-    );
-}
